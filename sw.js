96c562d455be956ade119457b80f62d947b1718f
Whiskeycolazero <willy.wilipedia@gmail.com>
2026-03-26 16:51:54 +0700

    add PWA files

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'icheck-v1';
+const FILES = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
